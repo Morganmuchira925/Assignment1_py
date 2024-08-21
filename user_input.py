a5a3c57bf93ee49df722ea86e147df7b823669ed
Morgan <morganisrael2005@gmail.com>
2024-08-21 11:30:06 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+name = input("Type in your name: ")
+print("Hello, " + name + "!")
+
+age = input("Type in your age: ")
+print("You are " + age + " years old.")
+
+location = input("Type in your current location: ")
+print(f"You live in {location}")
